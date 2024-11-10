86e0afd0003a46224d969cbf4201d61c013fd68c
ubuntu <ubuntu@ip-172-31-41-38>
2024-11-10 16:24:36 +0100

    web updated

--- eventos.sh
+++ eventos.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
-# Chequea qué tipo de instalador de paquetes utiliza e instala los paquetes si no los tiene ya
-
-paquetes='curl tor pandoc'
-
-apt --version &> /dev/null
-if [[ $(echo $?) == 0 ]]
-        then
-		apt update &> /dev/null
-                apt install -y $paquetes &> /dev/null
-fi
-
-dnf --version &> /dev/null
-if [[ $(echo $?) == 0 ]]
-        then
-		dnf install -y epel-release &> /dev/null
-                dnf install -y $paquetes &> /dev/null
-
-fi
-
-pacman -V &> /dev/null
-if [[ $(echo $?) == 0 ]]
-        then
-		sudo pacman -Sy &> /dev/null
-                sudo pacman -S --noconfirm $paquetes &> /dev/nulli
-fi
-
-# Ejecutamos tor en segundo plano para que se inicie
-#pkill tor &> /dev/null
-tor & > /dev/null 2>&1
-sleep 5
-
+#set -x
 ##Variables##
-rm -f /tmp/tmp.* 2> /dev/null
 file1="$(mktemp)"
 file2="$(mktemp)"
 futbol="$(mktemp)"
@@ -64,126 +33,116 @@ hora=[0-9]*:[0-9]*
 
 cd $(dirname $0)
 
-##Creación de ficheros##
-ls *.txt &> /dev/null
-if [ $? = 0 ]; then
-	rm -f *.txt
-else
-	touch "id_$date.txt"
-	touch "date_$date.txt"
-fi
-
+#Borramos anteriores archivos generados
+rm -f date*.txt
+rm -f eventos_sin_enlace.txt
 
 ## Descarga del fichero de enlaces y formateado de texto ##
-curl -m 20 -s --socks5-hostname localhost:9050 --retry 5 --retry-delay 5 https://elcano.top > $file1
+#curl -m 20 -s --socks5-hostname localhost:9050 --retry 5 --retry-delay 5 https://elcano.top > $file1
 
-awk '/• <a href="acestream/ {print $0}' $file1 | awk '{split($0,a,"\"")
-                                                split(a[5],b,">")
-                                                split(b[2],c,"<")
-                                                print a[2],"\""c[1]"\""}' > $file2
-
-while read line
-do
-        echo $line | awk '{split($0,a,"\"");print a[2],"\n"a[1]}' >> id_$date.txt
-
-done < $file2
+#Obtiene los ids y genera un archivo llamado channels2.txt
+#python ./getElcanoWeb.py #se ejecuta en el main.yml porque aqui da problemas
 
 # Organizador de enlaces
-canales_mliga="$(mktemp)"; grep -A1 -i -e "M\. *LaLiga *1080" -e "M\. *LaLiga *720" -e "M\..*Laliga *HD" id_$date.txt > $canales_mliga
-canales_mliga2="$(mktemp)"; grep -A1 -i -e 'M\. *LaLiga *2' id_$date.txt > $canales_mliga2
-canales_daznliga="$(mktemp)"; grep -A1 -i -e "dazn *laliga *1080" -e "dazn *laliga *720" id_$date.txt > $canales_daznliga
-canales_daznliga2="$(mktemp)"; grep -A1 -i -e 'dazn *laliga *2' id_$date.txt > $canales_daznliga2
-canales_liga_segunda="$(mktemp)"; grep -A1 -i -e 'smartbank 1080' -e 'smartbank 720' id_$date.txt > $canales_liga_segunda
-canales_liga_segunda2="$(mktemp)"; grep -A1 -i 'smartbank 2' id_$date.txt > $canales_liga_segunda2
-canales_liga_segunda3="$(mktemp)"; grep -A1 -i 'smartbank 3' id_$date.txt > $canales_liga_segunda3
-canales_dazn="$(mktemp)"; grep -A1 -i -e 'dazn *1' id_$date.txt > $canales_dazn
-canales_dazn2="$(mktemp)"; grep -A1 -i -e 'DAZN *2' id_$date.txt > $canales_dazn2
-canales_dazn3="$(mktemp)"; grep -A1 -i -e 'DAZN *3' id_$date.txt > $canales_dazn3
-canales_dazn4="$(mktemp)"; grep -A1 -i -e 'DAZN *4' id_$date.txt > $canales_dazn4
-canales_daznF1="$(mktemp)" ; grep -A1 -i -e 'DAZN *F1' id_$date.txt > $canales_daznF1
-canales_tvbar="$(mktemp)"; grep -A1 -i -e 'laliga *tv' id_$date.txt > $canales_tvbar
-canales_deportes="$(mktemp)" ; grep -A1 -i -e "m\. *deportes *1080" -e "m\. *deportes *720" id_$date.txt > $canales_deportes
-canales_deportes2="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *2' id_$date.txt > $canales_deportes2
-canales_deportes3="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *3' id_$date.txt > $canales_deportes3
-canales_deportes4="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *4' id_$date.txt > $canales_deportes4
-canales_deportes5="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *5' id_$date.txt > $canales_deportes5
-canales_deportes6="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *6' id_$date.txt > $canales_deportes6
-canales_deportes7="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *7' id_$date.txt > $canales_deportes7
-canales_campeones="$(mktemp)" ; grep -A1 -i -e "campeones *1080" -e "campeones *720" id_$date.txt > $canales_campeones
-canales_campeones2="$(mktemp)" ; grep -A1 -i -e "campeones *2" id_$date.txt > $canales_campeones2
-canales_campeones3="$(mktemp)" ; grep -A1 -i -e "campeones *3" id_$date.txt > $canales_campeones3
-canales_campeones4="$(mktemp)" ; grep -A1 -i -e "campeones *4" id_$date.txt > $canales_campeones4
-canales_campeones5="$(mktemp)" ; grep -A1 -i -e "campeones *5" id_$date.txt > $canales_campeones5
-canales_campeones6="$(mktemp)" ; grep -A1 -i -e "campeones *6" id_$date.txt > $canales_campeones6
-canales_campeones7="$(mktemp)" ; grep -A1 -i -e "campeones *7" id_$date.txt > $canales_campeones7
-canales_supercopa="$(mktemp)" ; grep -A1 -e 'SuperCopa' id_$date.txt > $canales_supercopa
-canales_vamos="$(mktemp)" ; grep -A1 -e 'Vamos' id_$date.txt > $canales_vamos
-canales_eurosport1="$(mktemp)" ; grep -A1 -i -e 'eurosport *1' id_$date.txt > $canales_eurosport1
-canales_eurosport2="$(mktemp)" ; grep -A1 -i -e 'eurosport *2' id_$date.txt > $canales_eurosport2
+canales_mliga="$(mktemp)"; grep -A1 -i -e "M\. *LaLiga *1080" -e "M\. *LaLiga *720" -e "M\..*Laliga *HD" channels2.txt > $canales_mliga
+canales_mliga2="$(mktemp)"; grep -A1 -i -e 'M\. *LaLiga *2' channels2.txt > $canales_mliga2
+canales_daznliga="$(mktemp)"; grep -A1 -i -e "dazn *laliga *1080" -e "dazn *laliga *720" channels2.txt > $canales_daznliga
+canales_daznliga2="$(mktemp)"; grep -A1 -i -e 'dazn *laliga *2' channels2.txt > $canales_daznliga2
+canales_liga_segunda="$(mktemp)"; grep -A1 -i -e 'smartbank 1080' -e 'smartbank 720' channels2.txt > $canales_liga_segunda
+canales_liga_segunda2="$(mktemp)"; grep -A1 -i 'smartbank 2' channels2.txt > $canales_liga_segunda2
+canales_liga_segunda3="$(mktemp)"; grep -A1 -i 'smartbank 3' channels2.txt > $canales_liga_segunda3
+canales_dazn="$(mktemp)"; grep -A1 -i -e 'dazn *1' channels2.txt > $canales_dazn
+canales_dazn2="$(mktemp)"; grep -A1 -i -e 'DAZN *2' channels2.txt > $canales_dazn2
+canales_dazn3="$(mktemp)"; grep -A1 -i -e 'DAZN *3' channels2.txt > $canales_dazn3
+canales_dazn4="$(mktemp)"; grep -A1 -i -e 'DAZN *4' channels2.txt > $canales_dazn4
+canales_daznF1="$(mktemp)" ; grep -A1 -i -e 'DAZN *F1' channels2.txt > $canales_daznF1
+canales_tvbar="$(mktemp)"; grep -A1 -i -e 'laliga *tv' channels2.txt > $canales_tvbar
+canales_deportes="$(mktemp)" ; grep -A1 -i -e "m\. *deportes *1080" -e "m\. *deportes *720" channels2.txt > $canales_deportes
+canales_deportes2="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *2' channels2.txt > $canales_deportes2
+canales_deportes3="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *3' channels2.txt > $canales_deportes3
+canales_deportes4="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *4' channels2.txt > $canales_deportes4
+canales_deportes5="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *5' channels2.txt > $canales_deportes5
+canales_deportes6="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *6' channels2.txt > $canales_deportes6
+canales_deportes7="$(mktemp)" ; grep -A1 -i -e 'm\. *deportes *7' channels2.txt > $canales_deportes7
+canales_campeones="$(mktemp)" ; grep -A1 -i -e "campeones *1080" -e "campeones *720" channels2.txt > $canales_campeones
+canales_campeones2="$(mktemp)" ; grep -A1 -i -e "campeones *2" channels2.txt > $canales_campeones2
+canales_campeones3="$(mktemp)" ; grep -A1 -i -e "campeones *3" channels2.txt > $canales_campeones3
+canales_campeones4="$(mktemp)" ; grep -A1 -i -e "campeones *4" channels2.txt > $canales_campeones4
+canales_campeones5="$(mktemp)" ; grep -A1 -i -e "campeones *5" channels2.txt > $canales_campeones5
+canales_campeones6="$(mktemp)" ; grep -A1 -i -e "campeones *6" channels2.txt > $canales_campeones6
+canales_campeones7="$(mktemp)" ; grep -A1 -i -e "campeones *7" channels2.txt > $canales_campeones7
+canales_supercopa="$(mktemp)" ; grep -A1 -e 'SuperCopa' channels2.txt > $canales_supercopa
+canales_vamos="$(mktemp)" ; grep -A1 -e 'Vamos' channels2.txt > $canales_vamos
+canales_eurosport1="$(mktemp)" ; grep -A1 -i -e 'eurosport *1' channels2.txt > $canales_eurosport1
+canales_eurosport2="$(mktemp)" ; grep -A1 -i -e 'eurosport *2' channels2.txt > $canales_eurosport2
 
 
 ##Descarga del fichero de horarios y formateado##
-curl -s https://www.futbolenlatv.es/competicion/la-liga | pandoc -f html > $futbol
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/la-liga | pandoc -f html > $futbol
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol > $filecanales
 
-curl -s https://www.futbolenlatv.es/competicion/segunda-division-espana | pandoc -f html > $futbol
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/segunda-division-espana | pandoc -f html > $futbol
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol >> $filecanales
 
-curl -s https://www.futbolenlatv.es/competicion/copa-del-rey | pandoc -f html > $futbol 
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/copa-del-rey | pandoc -f html > $futbol 
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol >> $filecanales
 
-curl -s https://www.futbolenlatv.es/competicion/supercopa-espana | pandoc -f html > $futbol
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/supercopa-espana | pandoc -f html > $futbol
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol >> $filecanales
 
-curl -s https://www.futbolenlatv.es/competicion/liga-campeones | pandoc -f html > $futbol
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/liga-campeones | pandoc -f html > $futbol
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol >> $filecanales
 
-curl -s https://www.futbolenlatv.es/competicion/europa-league | pandoc -f html > $futbol
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/europa-league | pandoc -f html > $futbol
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol >> $filecanales
 
-curl -s https://www.futbolenlatv.es/competicion/premier-league | pandoc -f html > $futbol
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/supercopa-europa | pandoc -f html > $futbol
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
 cat $futbol >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/baloncesto | pandoc -f html > $file3 
+curl -s --max-time 5 https://www.futbolenlatv.es/competicion/premier-league | pandoc -f html > $futbol
+sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $futbol > $canales_futbol; cat $canales_futbol | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $futbol
+cat $futbol >> $filecanales
+
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/baloncesto | pandoc -f html > $file3 
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $file3 > $basket; cat $basket | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $file3
 cat $file3 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/automovilismo | pandoc -f html > $file4
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/automovilismo | pandoc -f html > $file4
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $file4 > $formula; cat $formula | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $file4
 cat $file4 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/motociclismo | pandoc -f html > $file5 
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/motociclismo | pandoc -f html > $file5 
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $file5 > $motos; cat $motos | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $file5
 cat $file5 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/tenis | pandoc -f html > $file6
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/tenis | pandoc -f html > $file6
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $file6 > $tenis; cat $tenis | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $file6
 cat $file6 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/mma | pandoc -f html > $mma1
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/mma | pandoc -f html > $mma1
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $mma1 > $mma2; cat $mma2 | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $mma1
 cat $mma1 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/boxeo | pandoc -f html > $boxeo1
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/boxeo | pandoc -f html > $boxeo1
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $boxeo1 > $boxeo2; cat $boxeo2 | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $boxeo1
 cat $boxeo1 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/ciclismo | pandoc -f html > $ciclismo1
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/ciclismo | pandoc -f html > $ciclismo1
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $ciclismo1 > $ciclismo2; cat $ciclismo2 | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $ciclismo1
 cat $ciclismo1 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/padel | pandoc -f html > $padel1
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/padel | pandoc -f html > $padel1
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $padel1 > $padel2; cat $padel2 | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $padel1
 cat $padel1 >> $filecanales
 
-curl -s https://www.futbolenlatv.es/deporte/futbol-sala | pandoc -f html > $sala1
+curl -s --max-time 5 https://www.futbolenlatv.es/deporte/futbol-sala | pandoc -f html > $sala1
 sed -n '/Partidos de hoy/,/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/p' $sala1 > $sala2; cat $sala2 | head -n -1 | tail -n +2 | sed 's/<[^>]*>//g' > $sala1
 cat $sala1 >> $filecanales
 
@@ -214,7 +173,7 @@ sort $file8 > date_$date.txt # Ordena automaticamente de menor a mayor por la ho
 
 
 # Variables para FOR
-en=id_$date.txt
+en=channels2.txt
 can=date_$date.txt
 sed -i '/^ *$/d' $can # Eliminamos de nuevo lineas en blanco porque a veces por alguna razón todavía existe o se genera alguna magicamente
 
@@ -441,7 +400,7 @@ do
 	if [[ $(grep -i "teledeporte" $file1) ]]
         then
                echo "Teledeporte RTVE" >> $canales
-	       echo "https://www.rtve.es/play/videos/directo/deportes/tdp/" >> $canales
+	       echo "https://tdtchannels.com/television/Teledeporte" >> $canales
 	       canal_sin_enlace=false
 	fi
 
@@ -475,5 +434,4 @@ echo "</div>" >> index.html
 echo "</body>" >> index.html
 echo "</html>" >> index.html
 #cp -f index.html /var/www/html # Pon la ruta que quieras si quieres copiar o mover el archivo html y elimina el primer # de la linea
-
 
